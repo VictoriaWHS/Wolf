c114cccd8fe9df454e709124b2b6206be63153de
VictoriaWHS <victoria202975@student-schoolsofwestfield.org>
2025-01-09 17:25:19 +0000

    pages made

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-function wolfReal(){
-    Math.random(Math.floor)
+function goPen(){
+    window.location.replace("pen.html");
 }
 
-function home(){
+function wolves(){
+    let v=Math.random(Math.floor()*2);
+    console.log(" this is the number" + v);
+    //above is just to check its working//
+    if (v==1){
+        alert("wolf");
+        home();
+        }
+    else{
+        alert("no wolf");
+    } 
+}
 
+
+function home(){
+    alert("This is home");
 }
 
 function wolfChoice(){
-
+    alert("This is wolfChoice");
 }
 
 function village(){

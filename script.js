bababf80ea2c36defe6f24e07e8517617fbf5a4a
VictoriaWHS <victoria202975@student-schoolsofwestfield.org>
2025-01-13 17:09:08 +0000

    Done

--- script.js
+++ script.js
@@ -2,13 +2,9 @@
 let wolf =0;
 function wolves(){
     let wolf=Math.floor(Math.random()*2);
-    console.log(" this is the number" + wolf);
     if (wolf==1){
-        alert("wolf");
-        //not working//
         }
     else{
-        alert("no wolf");
     } 
     goPen();
 }
@@ -21,25 +17,16 @@ function goVillage(){
     window.location.replace("village.html");
     
 }
-function talkVillagers(){
-    window.location.replace("villager.html");
-}
-function goChoose(){
-    document.getElementById("wolfChoice").classList.remove("hide");
-}
-function goQuestion(){
-    window.location.replace("question.html");
-}
 
 
-
-
-function wolfChoice(){
-    alert("Wolf or no wolf");
+function wolfChoice(isWolf){
     //This is where there is the choice//
-    if (choice != "Go to pen"){
+    if (isWolf==true){
+        alert("Wolf")
     }
+    else alert("No wolf");
 }
+
 function village(){
     alert("This is village");
 }
